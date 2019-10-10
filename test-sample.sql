2fe7e493c3e9dca5f687d976b5f8f97cb89f8f56
Karen Ou <karenoujia@gmail.com>
2019-10-09 20:51:08 -0400

    Add update statement

--- test-sample.sql
+++ test-sample.sql
@@ -1,8 +1,14 @@
 SELECT s.semester_number,s.classid,s.pid,s.type
-FROM Semester AS s,(SELECT netid
+FROM Semester AS s,
+(SELECT netid
 FROM completed
 WHERE pid = 'Data' and type = 'Data Science Concentration') AS n
 WHERE s.netid = n.netid;
 
 INSERT INTO student
 VALUES ('jo99', 'Aspiring engineer interested in the intersection of business and technology');
+
+UPDATE completed
+SET type = 'Major'
+WHERE netid = 'dk194'
+AND pid = 'Psych';
