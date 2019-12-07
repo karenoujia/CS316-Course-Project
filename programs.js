5af55fc75c87e3cb9b42e435bba0c830397bbf70
man44duke <56346791+man44duke@users.noreply.github.com>
2019-12-07 17:46:07 -0500

    Create programs.js

---
+++ programs.js
@@ -0,0 +1,560 @@
+    {
+        "pid": 4,
+        "name": "Economics",
+        "type": "Minor"
+    },
+    {
+        "pid": 7,
+        "name": "Statistical Science",
+        "type": "Minor"
+    },
+    {
+        "pid": 10,
+        "name": "Computer Science",
+        "type": "Minor"
+    },
+    {
+        "pid": 17,
+        "name": "Chemistry",
+        "type": "Minor"
+    },
+    {
+        "pid": 21,
+        "name": "Physics",
+        "type": "Minor"
+    },
+    {
+        "pid": 24,
+        "name": "Mathematics",
+        "type": "Minor"
+    },
+    {
+        "pid": 27,
+        "name": "Biology",
+        "type": "Minor"
+    },
+    {
+        "pid": 30,
+        "name": "Earth & Ocean Sciences",
+        "type": "Minor"
+    },
+    {
+        "pid": 33,
+        "name": "Environmental Sciences & Policy",
+        "type": "Minor"
+    },
+    {
+        "pid": 35,
+        "name": "Dance",
+        "type": "Minor"
+    },
+    {
+        "pid": 37,
+        "name": "History",
+        "type": "Minor"
+    },
+    {
+        "pid": 40,
+        "name": "African & African American Studies",
+        "type": "Minor"
+    },
+    {
+        "pid": 42,
+        "name": "Asian & Middle Eastern Studies",
+        "type": "Minor"
+    },
+    {
+        "pid": 44,
+        "name": "Art History",
+        "type": "Minor"
+    },
+    {
+        "pid": 46,
+        "name": "Brazilian & Global Portuguese",
+        "type": "Minor"
+    },
+    {
+        "pid": 48,
+        "name": "Classical Archaeology",
+        "type": "Minor"
+    },
+    {
+        "pid": 49,
+        "name": "Classical Civilization",
+        "type": "Minor"
+    },
+    {
+        "pid": 52,
+        "name": "Cultural Anthropology",
+        "type": "Minor"
+    },
+    {
+        "pid": 54,
+        "name": "Education",
+        "type": "Minor"
+    },
+    {
+        "pid": 55,
+        "name": "Electrical and Computer Engineering",
+        "type": "Minor"
+    },
+    {
+        "pid": 56,
+        "name": "Elementary Teacher Preparation",
+        "type": "Minor"
+    },
+    {
+        "pid": 57,
+        "name": "Secondary Teacher Preparation",
+        "type": "Minor"
+    },
+    {
+        "pid": 58,
+        "name": "Creative Writing",
+        "type": "Minor"
+    },
+    {
+        "pid": 59,
+        "name": "English",
+        "type": "Minor"
+    },
+    {
+        "pid": 61,
+        "name": "Evolutionary Anthropology",
+        "type": "Minor"
+    },
+    {
+        "pid": 63,
+        "name": "Evolutionary Anthropology",
+        "type": "BS"
+    },
+    {
+        "pid": 65,
+        "name": "French Studies",
+        "type": "Minor"
+    },
+    {
+        "pid": 66,
+        "name": "German",
+        "type": "Minor"
+    },
+    {
+        "pid": 68,
+        "name": "Global Cultural Studies",
+        "type": "Minor"
+    },
+    {
+        "pid": 70,
+        "name": "Global Health",
+        "type": "Minor"
+    },
+    {
+        "pid": 71,
+        "name": "Global Health",
+        "type": "Minor"
+    },
+    {
+        "pid": 72,
+        "name": "Greek",
+        "type": "Minor"
+    },
+    {
+        "pid": 73,
+        "name": "Gender, Sexuality, and Feminist Studies",
+        "type": "Minor"
+    },
+    {
+        "pid": 75,
+        "name": "Sexuality Studies",
+        "type": "Minor"
+    },
+    {
+        "pid": 77,
+        "name": "Italian Studies",
+        "type": "Minor"
+    },
+    {
+        "pid": 79,
+        "name": "Latin",
+        "type": "Minor"
+    },
+    {
+        "pid": 80,
+        "name": "Linguistics",
+        "type": "Minor"
+    },
+    {
+        "pid": 82,
+        "name": "Mathematics",
+        "type": "Minor"
+    },
+    {
+        "pid": 84,
+        "name": "Music",
+        "type": "Minor"
+    },
+    {
+        "pid": 86,
+        "name": "Neuroscience",
+        "type": "Minor"
+    },
+    {
+        "pid": 88,
+        "name": "Neuroscience",
+        "type": "BS"
+    },
+    {
+        "pid": 89,
+        "name": "Philosophy",
+        "type": "Minor"
+    },
+    {
+        "pid": 91,
+        "name": "Political Science",
+        "type": "Minor"
+    },
+    {
+        "pid": 94,
+        "name": "Religious Studies",
+        "type": "Minor"
+    },
+    {
+        "pid": 97,
+        "name": "Russian and East European Literatures in Translation",
+        "type": "Minor"
+    },
+    {
+        "pid": 98,
+        "name": "Russian Culture & Language",
+        "type": "Minor"
+    },
+    {
+        "pid": 100,
+        "name": "Polish Culture & Language",
+        "type": "Minor"
+    },
+    {
+        "pid": 101,
+        "name": "Medical Sociology",
+        "type": "Minor"
+    },
+    {
+        "pid": 102,
+        "name": "Sociology",
+        "type": "Minor"
+    },
+    {
+        "pid": 104,
+        "name": "Spanish Studies",
+        "type": "Minor"
+    },
+    {
+        "pid": 106,
+        "name": "Theater Studies",
+        "type": "Minor"
+    },
+    {
+        "pid": 108,
+        "name": "Photography",
+        "type": "Minor"
+    },
+    {
+        "pid": 109,
+        "name": "Visual & Media Studies",
+        "type": "Minor"
+    },
+    {
+        "pid": 111,
+        "name": "Visual Arts",
+        "type": "Minor"
+    },
+    {
+        "pid": 113,
+        "name": "Biomedical Engineering",
+        "type": "BSE"
+    },
+    {
+        "pid": 114,
+        "name": "Civil Engineering",
+        "type": "BSE"
+    },
+    {
+        "pid": 115,
+        "name": "Environmental Engineering",
+        "type": "BSE"
+    },
+    {
+        "pid": 116,
+        "name": "Mechanical Engineering",
+        "type": "BSE"
+    },
+    {
+        "pid": 2,
+        "name": "Economics",
+        "type": "BS"
+    },
+    {
+        "pid": 6,
+        "name": "Statistical Science",
+        "type": "BS"
+    },
+    {
+        "pid": 9,
+        "name": "Computer Science",
+        "type": "BS"
+    },
+    {
+        "pid": 12,
+        "name": "Psychology",
+        "type": "BS"
+    },
+    {
+        "pid": 16,
+        "name": "Chemistry",
+        "type": "BS"
+    },
+    {
+        "pid": 20,
+        "name": "Physics",
+        "type": "BS"
+    },
+    {
+        "pid": 23,
+        "name": "Mathematics",
+        "type": "BS"
+    },
+    {
+        "pid": 26,
+        "name": "Biology",
+        "type": "BS"
+    },
+    {
+        "pid": 29,
+        "name": "Earth & Ocean Sciences",
+        "type": "BS"
+    },
+    {
+        "pid": 31,
+        "name": "Environmental Sciences",
+        "type": "BS"
+    },
+    {
+        "pid": 41,
+        "name": "African & African American Studies",
+        "type": "BA"
+    },
+    {
+        "pid": 43,
+        "name": "Asian & Middle Eastern Studies",
+        "type": "BA"
+    },
+    {
+        "pid": 45,
+        "name": "Art History",
+        "type": "BA"
+    },
+    {
+        "pid": 47,
+        "name": "Brazilian & Global Portuguese",
+        "type": "BA"
+    },
+    {
+        "pid": 50,
+        "name": "Classical Civilization",
+        "type": "BA"
+    },
+    {
+        "pid": 51,
+        "name": "Classical Languages",
+        "type": "BA"
+    },
+    {
+        "pid": 53,
+        "name": "Cultural Anthropology",
+        "type": "BA"
+    },
+    {
+        "pid": 60,
+        "name": "English",
+        "type": "BA"
+    },
+    {
+        "pid": 62,
+        "name": "Evolutionary Anthropology",
+        "type": "BA"
+    },
+    {
+        "pid": 64,
+        "name": "French & Francophone Studies",
+        "type": "BA"
+    },
+    {
+        "pid": 67,
+        "name": "German",
+        "type": "BA"
+    },
+    {
+        "pid": 69,
+        "name": "Global Cultural Studies",
+        "type": "BA"
+    },
+    {
+        "pid": 74,
+        "name": "Gender, Sexuality, and Feminist Studies",
+        "type": "BA"
+    },
+    {
+        "pid": 76,
+        "name": "International Comparative Studies",
+        "type": "BA"
+    },
+    {
+        "pid": 78,
+        "name": "Italian Studies",
+        "type": "BA"
+    },
+    {
+        "pid": 81,
+        "name": "Linguistics",
+        "type": "BA"
+    },
+    {
+        "pid": 83,
+        "name": "Medieval & Renaissance Studies",
+        "type": "BA"
+    },
+    {
+        "pid": 85,
+        "name": "Music",
+        "type": "BA"
+    },
+    {
+        "pid": 87,
+        "name": "Neuroscience",
+        "type": "BA"
+    },
+    {
+        "pid": 90,
+        "name": "Philosophy",
+        "type": "BA"
+    },
+    {
+        "pid": 92,
+        "name": "Political Science",
+        "type": "BA"
+    },
+    {
+        "pid": 93,
+        "name": "Public Policy Studies",
+        "type": "BA"
+    },
+    {
+        "pid": 95,
+        "name": "Religious Studies",
+        "type": "BA"
+    },
+    {
+        "pid": 96,
+        "name": "Romance Studies",
+        "type": "BA"
+    },
+    {
+        "pid": 99,
+        "name": "Russian",
+        "type": "BA"
+    },
+    {
+        "pid": 1,
+        "name": "Economics",
+        "type": "BA"
+    },
+    {
+        "pid": 5,
+        "name": "Statistical Science",
+        "type": "BA"
+    },
+    {
+        "pid": 8,
+        "name": "Computer Science",
+        "type": "BA"
+    },
+    {
+        "pid": 11,
+        "name": "Psychology",
+        "type": "BA"
+    },
+    {
+        "pid": 15,
+        "name": "Chemistry",
+        "type": "BA"
+    },
+    {
+        "pid": 19,
+        "name": "Physics",
+        "type": "BA"
+    },
+    {
+        "pid": 22,
+        "name": "Mathematics",
+        "type": "BA"
+    },
+    {
+        "pid": 25,
+        "name": "Biology",
+        "type": "BA"
+    },
+    {
+        "pid": 28,
+        "name": "Earth & Ocean Sciences",
+        "type": "BA"
+    },
+    {
+        "pid": 32,
+        "name": "Environmental Sciences & Policy",
+        "type": "BA"
+    },
+    {
+        "pid": 34,
+        "name": "Dance",
+        "type": "BA"
+    },
+    {
+        "pid": 36,
+        "name": "History",
+        "type": "BA"
+    },
+    {
+        "pid": 38,
+        "name": "Slavic & Eurasian Studies",
+        "type": "BA"
+    },
+    {
+        "pid": 3,
+        "name": "Economics",
+        "type": "BS, Finance Concentration"
+    },
+    {
+        "pid": 103,
+        "name": "Sociology",
+        "type": "BA"
+    },
+    {
+        "pid": 105,
+        "name": "Spanish, Latin American & Latino/a Studies",
+        "type": "BA"
+    },
+    {
+        "pid": 107,
+        "name": "Theater Studies",
+        "type": "BA"
+    },
+    {
+        "pid": 110,
+        "name": "Visual & Media Studies",
+        "type": "BA"
+    },
+    {
+        "pid": 112,
+        "name": "Visual Arts",
+        "type": "BA"
+    }
